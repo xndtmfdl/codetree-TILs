199cecf42d1c9e721e666948c2c4fd33c939bb6d
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-10-05 15:07:57 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241005/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a;
+    cin >> a;
+    cout << fixed;
+    cout.precision(1);
+    cout << a * 30.48;
+    return 0;
+}
