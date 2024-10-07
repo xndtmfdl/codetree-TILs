fb3e701d8fbce5596abd31b5e0851c2e79a21c6f
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-10-07 20:28:10 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241007/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    char c;
+    cin >> a;
+    cin >> c;
+    cin >> b;
+    cout << a+1 << c << b;
+    return 0;
+}
