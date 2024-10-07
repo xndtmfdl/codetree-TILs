15742d7ccc0621d6b0cb5ba73acbbe676c28077f
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-10-07 20:30:50 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241007/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+    cout << c << "." << a << "." << b;
+    return 0;
+}
