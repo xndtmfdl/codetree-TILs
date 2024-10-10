8d0ef6b5deab2504503b40ed7eb86a3d8b8396dc
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-10-10 15:21:31 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+    cout << "010-" << c << "-" << b;
+    return 0;
+}
