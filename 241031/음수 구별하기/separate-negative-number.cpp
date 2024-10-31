93e6512d9aa4e1c83afe3263d9961a60853ed3e9
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-10-31 17:15:40 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241031/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    cout << n << endl;
+    if (n<0)
+        cout << "minus";
+    return 0;
+}
