daa3ac5ada9624c75b4ccd5de574334c5d214c7a
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-07 16:03:54 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241107/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a;
+    cin >> a;
+    if(a >= 1)
+        cout << "High";
+    else if (a >0.5)
+        cout << "Middle";
+    else
+        cout << "Low";
+    return 0;
+}
