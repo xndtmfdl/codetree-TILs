87976d4b18808a4edf80c296cf7bdd2b60c6e176
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-14 15:36:14 +0900

    Title: 출석 부르기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241114/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    if(a==1)
+        cout << "John";
+    else if (a==2)
+        cout << "Tom";
+    else if(a==3)
+        cout << "Paul";
+    else 
+        cout << " Vacancy";
+    return 0;
+}
