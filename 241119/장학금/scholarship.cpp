10c837b70fc4e4576d51f5b135d98636f7d74725
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-19 17:33:20 +0900

    Title: 장학금, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241119/장학금/scholarship.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    if(a>=90 && b<=100) {
+        if(b>=95)
+            cout << "100000";
+        else if(b>=90)
+            cout << "50000";
+    }
+    else    
+        cout << "0";
+    return 0;
+}
