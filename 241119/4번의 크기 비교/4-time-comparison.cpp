9393e87f838d2efe0e075bca616a1e2128347377
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-19 15:48:04 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241119/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    cout << (a>b) << endl;
+    cout << (a>c) << endl;
+    cout << (a>d) << endl;
+    cout << (a>e) << endl;
+    
+    return 0;
+}
