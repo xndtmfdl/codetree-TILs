710a841bc18791d7ff218ffd5caf0759a3899808
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-19 17:26:25 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241119/굉장한 숫자/amazing-number.cpp
+++ 241119/굉장한 숫자/amazing-number.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    if((n%3==0 && n%2!=0) || (n%2==0 %% n%5==0))
+    if((n%3==0 && n%2!=0) || (n%2==0 && n%5==0))
         cout << "true";
     else
         cout << "false";
