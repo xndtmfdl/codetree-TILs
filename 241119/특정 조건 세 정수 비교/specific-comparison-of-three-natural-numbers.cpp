3831325bc4301b788f6d5f6009737ad7931d5c72
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-19 17:11:35 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241119/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 241119/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -5,8 +5,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c;
     cin >> a >> b >> c;
-    if(a<b) {
-        cout << (a<c);
+    if(a<=b) {
+        cout << (a<=c);
     }
     else   
         cout << "0";
