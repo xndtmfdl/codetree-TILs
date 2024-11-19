5d19bf2541b3afb98abfbf7e78e55ce3f77bf297
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-19 17:25:23 +0900

    Title: 굉장한 숫자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241119/굉장한 숫자/amazing-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    if((n%3==0 && n%2!=0) || (n%2==0 %% n%5==0))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
