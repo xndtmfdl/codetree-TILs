2346597f59614726744e20647df95c908dc33f89
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-21 15:37:54 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241121/윤년인가/is-leap-year.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    if((a%4!=0 && a%400!=0))
+        cout << "false";
+    else
+        cout << "true";
+    return 0;
+}
