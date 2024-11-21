a60e7175099ea4234637566142eb36a4df9ac54d
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-21 16:41:57 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241121/중앙값 구하기/find-the-median.cpp
+++ 241121/중앙값 구하기/find-the-median.cpp
@@ -7,9 +7,9 @@ int main() {
     cin >> a >> b >> c;
     if((a < b && b <c)||c<b && b<a)
         cout << b;
-    else if(() b<c && c<a)||(a<c && c< b))
+    else if(( b<c && c<a)||(a<c && c< b))
         cout << c;
-    else if()
+    else
         cout << a;
     return 0;
 }
