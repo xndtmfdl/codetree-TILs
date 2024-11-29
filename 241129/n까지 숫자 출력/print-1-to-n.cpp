2834c16feaa18a29359b582cbfa93d3417ad5659
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2024-11-29 19:38:18 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241129/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, i=1;
+    cin >> n;
+    while (i<=n) {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
