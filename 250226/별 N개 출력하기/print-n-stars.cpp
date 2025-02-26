a030c46c9c9891a1888f51faa9082efbdd85d461
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-02-26 16:37:17 +0900

    Title: 별 N개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250226/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n=0;
+    cin >> n;
+    int cnt = 0;
+    while(cnt<n) {
+        cnt++;
+        cout << "*" << "\n";
+    }
+
+    return 0;
+}
