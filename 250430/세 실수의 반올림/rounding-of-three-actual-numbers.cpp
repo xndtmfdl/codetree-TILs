3a9f56073311edac1e614be9efa4993b9359ed8f
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-04-30 23:17:18 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250430/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout.precision(3);
+    cout << a << '\n' << b << '\n' << c;
+    return 0;
+}
