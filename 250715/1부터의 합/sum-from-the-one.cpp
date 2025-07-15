2ba29660297567cbdd5c22c438639dac82386c9a
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-15 14:05:59 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250715/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int sum=0;
+    for(int i=1; i<=100; i++) {
+        sum+=i;
+        if(sum>=n) {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}
