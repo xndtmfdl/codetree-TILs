20590df5d9979bdc2729da50afeffa097d412fed
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-18 00:11:09 +0900

    Title: 증가시키며 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250718/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n=10;
+    while (n<27) {
+        cout << n << " ";
+        n+=2;
+    }
+    return 0;
+}
