c5eace223b50109c289982418bfca099e7937239
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-19 23:52:08 +0900

    Title: 감소시키며 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250719/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i = 26;
+    while(i>=10) {
+        cout << i << " ";
+        i--;
+    }
+    return 0;
+}
