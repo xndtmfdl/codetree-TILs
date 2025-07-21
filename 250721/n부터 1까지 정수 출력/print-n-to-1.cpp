1bb94f978968bb1f752c3d147fa475b0c8533989
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-21 14:05:40 +0900

    Title: n부터 1까지 정수 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    while(n>0) {
+        cout << n << " ";
+        n--;
+    }
+    return 0;
+}
