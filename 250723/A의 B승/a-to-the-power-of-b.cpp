cbc728ab44836d2de8f4ee3451b9a2b11d9b4b74
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-23 15:25:06 +0900

    Title: A의 B승, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250723/A의 B승/a-to-the-power-of-b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    int prod=1;
+    for(int i=0; i<b; i++) {
+        prod*=a;
+    }
+    cout << prod;
+    return 0;
+}
