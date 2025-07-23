d8ce5d186dbdc521f50797d49611d96360c8f40d
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-23 12:57:30 +0900

    Title: 숫자의 증감, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250723/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    int n;
+    cin >> c >> n;
+    if(c=='A') {
+        for(int i=1; i<=n; i++) {
+            cout << i << " ";
+        }
+    }
+    else if(c == 'D') {
+        for(int i=n; i>=1; i--) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
