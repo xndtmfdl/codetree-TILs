68e611b761541e8d76444cc2aacd51c4dc4fc205
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-23 13:47:40 +0900

    Title: 100부터 입력받는 수까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250723/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, sum=0;
+    cin >> n;
+    for(int i=n; i<=100; i++) {
+        sum+=i;
+    }
+    cout << sum;''
+    return 0;
+}
