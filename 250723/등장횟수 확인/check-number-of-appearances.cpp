5cb61d5b3e7de738d444e80f400f0b15d2ebc669
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-23 13:44:57 +0900

    Title: 등장횟수 확인, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250723/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, cnt=0;
+    for(int i=0; i<5; i++) {
+        cin >> a;
+        if(a%2==0) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
