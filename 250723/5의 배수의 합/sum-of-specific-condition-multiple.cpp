5a338c059ffcd986cdbad67a18af1a84a968ade0
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-23 15:01:57 +0900

    Title: 5의 배수의 합, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250723/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, sum=0;
+    cin >> a >> b;
+    if(a>b) {
+        for(int i=b; i<=a; i++) {
+            if(i%5==0) {
+              sum+=i;
+            }   
+        }
+    }
+    else {
+        for(int i=a; i<=b; i++) {
+        if(i%5==0) {
+            sum+=i;
+        }
+    }
+    }
+    cout << sum;
+    return 0;
+}
