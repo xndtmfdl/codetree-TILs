34e1b04ec0a3c80354d1f28d4c8b81d8af341ea4
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-24 14:30:38 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250724/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    while(1) {
+        cin >> a;
+        if(a==0) break;
+        cout << a << "\n";
+        
+    }
+    return 0;
+}
