209134db7d4208a301948b3c84307e0a25af0f0c
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-07-29 14:49:08 +0900

    Title: 별표 출력하기 5, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250729/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for(int i=n; i>0; i--) {
+        for (int j=0; j<i; j++) {
+            for(int k=0; k<i; k++) {
+                cout << "*";
+            }
+            cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
