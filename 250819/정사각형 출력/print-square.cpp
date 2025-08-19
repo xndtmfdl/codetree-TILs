a125111f42d3e55ea371516b8cf0fac0b4bd05de
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-08-19 12:33:10 +0900

    Title: 정사각형 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250819/정사각형 출력/print-square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int cnt=1, n;
+    cin >>n;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cout << cnt << " ";
+            cnt++; 
+        }
+        cout << "\n";
+    }
+    return 0;
+}
