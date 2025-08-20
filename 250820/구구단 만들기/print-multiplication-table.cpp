61622225c1b2e4978c07ab9884efa55943c2b250
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-08-20 14:37:04 +0900

    Title: 구구단 만들기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250820/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    for(int j=1; j<=9; j++) {
+        for(int i=b; i>=a; i--) {
+            if(i%2==0) {
+                cout << i << " * " << j << " = " << i*j << " ";
+            if(i!=a) {
+                cout << "/ ";
+            }
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
