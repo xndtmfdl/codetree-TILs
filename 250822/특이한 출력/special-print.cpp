7e57292446dc126c0169b7cff8c1b31f567abec9
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-08-22 15:25:06 +0900

    Title: 특이한 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250822/특이한 출력/special-print.cpp
+++ 250822/특이한 출력/special-print.cpp
@@ -8,7 +8,7 @@ int main() {
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n; j++) {
             cout << "(" << i << ", " << j << ") ";
-            if((i+j)==4) cout << "\n";
+            if((i+j)%4==0) cout << "\n";
         }
     }
     return 0;
