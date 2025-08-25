e843e6451179a256b2714a7038a3f2cf3c52d2a8
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-08-25 12:43:54 +0900

    Title: 알파벳 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250825/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt=65;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<=i; j++) {
+            cout << (char)cnt;
+            cnt++;
+            if((char)cnt=='[') cnt=65;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
