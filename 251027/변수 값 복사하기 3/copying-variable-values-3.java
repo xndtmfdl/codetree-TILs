655762ee61d398556c659ea3c0a02be9bd72c864
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-10-27 08:52:30 +0900

    Title: 변수 값 복사하기 3, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251027/변수 값 복사하기 3/copying-variable-values-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1, b = 5, c = 3;
+        a = c;
+        a = a+c;
+        b = b-c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
