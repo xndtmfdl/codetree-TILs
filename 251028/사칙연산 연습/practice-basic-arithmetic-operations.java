c0192617733f94545cdc43b7f5a1a0f7556a8c94
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-10-28 08:41:04 +0900

    Title: 사칙연산 연습, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251028/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 30;
+        int c = a+b;
+        System.out.println(c/8 + "\n" + c%8);
+        
+    }
+}
