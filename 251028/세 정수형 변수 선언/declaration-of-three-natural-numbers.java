36a7be3c1e6bdea4052b6ddc9059efa06621cf3e
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-10-28 08:45:38 +0900

    Title: 세 정수형 변수 선언, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251028/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 7, b = 23, c = 30;
+        System.out.println(a + " + " + b + " = " + c);
+    }
+}
