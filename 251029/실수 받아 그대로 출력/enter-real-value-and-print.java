1059993da6c30bb526cfc124628be079a1e87eca
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-10-29 13:55:31 +0900

    Title: 실수 받아 그대로 출력, Time: 138ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251029/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double n = sc.nextDouble();
+		System.out.printf("%.2f", n);
+		sc.close();
+		
+	}
+}
