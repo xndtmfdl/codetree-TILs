2b12e27bde16f1025a0714f2179cca2898cd640d
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-11-03 08:36:23 +0900

    Title: 문자 출력하기, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251103/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		for(int i=0; i<8; i++) {
+			System.out.print(a);
+		}
+		
+	}
+}
