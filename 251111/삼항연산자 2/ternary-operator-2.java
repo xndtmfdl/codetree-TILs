1b44fb60ef71d7ec3e2e09e79335bd3bc545ddc0
xndtmfdl <166918313+xndtmfdl@users.noreply.github.com>
2025-11-11 08:48:38 +0900

    Title: 삼항연산자 2, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251111/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// Please write your code here.
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println((a==1) ? 't' : 'f');
+		
+	}
+}
